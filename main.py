5ddf47c0d0c73cc3c32379558e393c231bbdfd4e
siddharth shah <siddharthshah3030@gmail.com>
2019-04-23 17:31:33 +0530

    scraping whole list successfully

--- main.py
+++ main.py
@@ -4,30 +4,42 @@ import csv
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-url = "https://www.zaubacorp.com/company/DR-REDDY-S-LABORATORIES-LTD/L85195TG1984PLC004507"
-html = urlopen(url)
-soup = BeautifulSoup(html, 'lxml')
-type(soup)
 
 dense = 5
 
-def searchDepth():
+def searchDepth(url,depth):
+    url = "https://www.zaubacorp.com/company/DR-REDDY-S-LABORATORIES-LTD/L85195TG1984PLC004507"
+    html = urlopen(url)
+    soup = BeautifulSoup(html, 'lxml')
+    type(soup)
+
     depth = 1
     index = 1
     layerEnd = False
     while layerEnd == False:
-        directorRow = soup.find("tr", { "id" : "package1" })
-        director = directorRow.findAll("td")
-        URL = director[1].p.a.get('href')
-        DIN = director[0].p.contents[0]
-        directorName = director[1].p.a.contents[0]
-        designation =  director[2].p.contents[0]
-        appointmentDate =  director[3].p.contents[0]
-        print(URL)
-        writeCSV(URL,DIN,directorName,designation,appointmentDate,depth)
+        directorRow = soup.find("tr", { "id" : "package"+str(index) })
+        if not directorRow :
+            break
+        #directorDetails(directorRow ,depth)
+        next_td_tag = directorRow.findNext('table').tr.findNext("tr").td.p.a.get("href")
+        print(next_td_tag)        
+        index = index +1
+
+
+        
+def directorDetails(directorRow,depth):
+    director = directorRow.findAll("td")
+    URL = director[1].p.a.get('href')
+    DIN = director[0].p.contents[0]
+    directorName = director[1].p.a.contents[0]
+    designation =  director[2].p.contents[0]
+    appointmentDate =  director[3].p.contents[0]
+    print(URL)
+    writeCSV(URL,DIN,directorName,designation,appointmentDate,depth)
+        
 def writeCSV(a,b,c,d,e,f):
     row = [a,b,c,d,e,f]            
-    with open('people2.csv', 'a') as csvFile:
+    with open('directorDepth'+str(f)+'.csv', 'a') as csvFile:
         writer = csv.writer(csvFile)
         writer.writerow(row)
     csvFile.close()    
